4be0373352bf0140b3249d2928ca49d369fb9fb2
Posey <lpose22@gmail.com>
2019-01-28 15:39:50 -0600

    fix an oops

--- Python/models/scatter.py
+++ Python/models/scatter.py
@@ -2,7 +2,6 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
-# Load the amd dataset
 df = pd.read_csv("file_path.csv")
 df.plot()  # plots all columns against index
 df.plot(kind='scatter',x='x',y='y') # scatter plot
