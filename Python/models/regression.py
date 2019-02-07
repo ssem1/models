31c8a29668c1d6fd461e90479dc22613157e9ef7
Poseyy <lpose22@gmail.com>
2019-02-06 20:46:15 -0600

    testing for outputting as JSON

---
+++ Python/models/regression.py
@@ -0,0 +1,40 @@
+import quandl
+import pandas as pd
+import numpy as np
+import datetime
+import matplotlib.pyplot as plt
+
+from sklearn.linear_model import LinearRegression
+from sklearn import preprocessing, svm
+from sklearn.model_selection import train_test_split
+
+import json
+
+testdata = quandl.get("WIKI/GOOG")
+testdata = testdata[['Adj. Close']]
+
+forecast_out = int(90)
+testdata['Prediction'] = testdata[['Adj. Close']].shift(-forecast_out)
+
+print(testdata.tail())
+X = np.array(testdata.drop(['Prediction'], 1))
+X = preprocessing.scale(X)
+X_forecast = X[-forecast_out:] # set X_forecast equal to last 30
+X = X[:-forecast_out] # remove last 30 from X
+y = np.array(testdata['Prediction'])
+y = y[:-forecast_out]
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2)
+# Training
+clf = LinearRegression()
+clf.fit(X_train,y_train)
+# Testing
+confidence = clf.score(X_test, y_test)
+print("confidence: ", confidence)
+forecast_prediction = clf.predict(X_forecast)
+print(forecast_prediction)
+list_prediction = forecast_prediction.tolist()  # plots all columns against index
+plt.plot(list_prediction)
+print(testdata)
+with open('data.json', 'w') as outfile:
+    json.dump(list_prediction, outfile)
+plt.show()
